d0cf0049f163701202bee092f952d99a36321f4c
Clementine Allen <tomtinderbundle@gmail.com>
2023-02-01 16:29:41 -0800

    Made RobotMap

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+public class RobotMap {
+    public static final class Pneumatics {
+        public static final int PCM = 41;
+        public static final PneumaticsModuleType PCM_TYPE = PneumaticsModuleType.CTREPCM;
+        public static final int INTAKE_DEPLOY_PCM_CHANNEL = 0;
+    }
+
+}
