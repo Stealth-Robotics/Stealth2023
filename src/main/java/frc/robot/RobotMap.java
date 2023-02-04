7e701f3cb10f741756cd446c04fe752da4b5bf18
Sidd Rao <siddrao.926@gmail.com>
2023-02-04 15:22:39 -0800

    Moved RobotMap Constants To RobotMap File

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,6 +3,32 @@ package frc.robot;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class RobotMap {
+    public static final class Drivebase {
+        public static final class Mod0 {
+            public static final int driveMotorID = 1;
+            public static final int angleMotorID = 2;
+            public static final int canCoderID = 1;
+        }
+
+        public static final class Mod1 {
+            public static final int driveMotorID = 3;
+            public static final int angleMotorID = 4;
+            public static final int canCoderID = 2;
+        }
+
+        public static final class Mod2 {
+            public static final int driveMotorID = 5;
+            public static final int angleMotorID = 6;
+            public static final int canCoderID = 3;
+        }
+
+        public static final class Mod3 {
+            public static final int driveMotorID = 7;
+            public static final int angleMotorID = 8;
+            public static final int canCoderID = 4;
+        }
+    }
+
     public static final class Pneumatics {
         public static final int PCM = 41;
         public static final PneumaticsModuleType PCM_TYPE = PneumaticsModuleType.CTREPCM;
