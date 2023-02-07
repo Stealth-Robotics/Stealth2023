3dd4ca98215a4ec04792a9f4e05f87c5a0d24a03
Clementine Allen <tomtinderbundle@gmail.com>
2023-02-06 17:27:21 -0800

    copied and pasted lasts years path follower, with edits to work on our robot and swerve code.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,8 +10,10 @@ import edu.wpi.first.math.util.Units;
 import frc.lib.util.COTSFalconSwerveConstants;
 import frc.lib.util.SwerveModuleConstants;
 
+
 public final class Constants {
     public static final double stickDeadband = 0.1;
+
 
     public static final class Swerve {
         public static final int pigeonID = 1;
@@ -145,4 +147,8 @@ public final class Constants {
             new TrapezoidProfile.Constraints(
                 kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
     }
+
+    
+
+
 }
