61098128c63e5add13aa9e4f44994057af49ee33
oliverstivers <oliverstivers@hotmail.com>
2023-02-09 13:32:35 -0800

    reformatting and added slow mode + current pose readout

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -10,15 +10,16 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-
-public class TeleopSwerve extends CommandBase {    
-    private Swerve s_Swerve;    
+public class TeleopSwerve extends CommandBase {
+    private Swerve s_Swerve;
     private DoubleSupplier translationSup;
     private DoubleSupplier strafeSup;
     private DoubleSupplier rotationSup;
     private BooleanSupplier robotCentricSup;
+    private BooleanSupplier slowModeSup;
 
-    public TeleopSwerve(Swerve s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
+    public TeleopSwerve(Swerve s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup,
+            DoubleSupplier rotationSup, BooleanSupplier robotCentricSup, BooleanSupplier slowModeSup) {
         this.s_Swerve = s_Swerve;
         addRequirements(s_Swerve);
 
@@ -26,21 +27,22 @@ public class TeleopSwerve extends CommandBase {
         this.strafeSup = strafeSup;
         this.rotationSup = rotationSup;
         this.robotCentricSup = robotCentricSup;
+        this.slowModeSup = slowModeSup;
     }
 
     @Override
     public void execute() {
-        /* Get Values, Deadband*/
+        /* Get Values, Deadband */
         double translationVal = MathUtil.applyDeadband(translationSup.getAsDouble(), Constants.stickDeadband);
         double strafeVal = MathUtil.applyDeadband(strafeSup.getAsDouble(), Constants.stickDeadband);
         double rotationVal = MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.stickDeadband);
 
         /* Drive */
         s_Swerve.drive(
-            new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed), 
-            rotationVal * Constants.Swerve.maxAngularVelocity, 
-            !robotCentricSup.getAsBoolean(), 
-            true
-        );
+                new Translation2d(translationVal, strafeVal)
+                        .times(Constants.Swerve.maxSpeed * (slowModeSup.getAsBoolean() ? 0.5 : 1)),
+                rotationVal * Constants.Swerve.maxAngularVelocity * (slowModeSup.getAsBoolean() ? 0.5 : 1),
+                !robotCentricSup.getAsBoolean(),
+                true);
     }
 }
