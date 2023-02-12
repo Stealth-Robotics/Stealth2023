d13517e9a5b78f36f86183c90e7f78c1042eec33
oliverstivers <oliverstivers@gmail.com>
2023-02-12 11:56:50 -0800

    added basic claw substystem and info in robotmap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,6 +7,7 @@ public class RobotMap {
         public static final int PCM = 41;
         public static final PneumaticsModuleType PCM_TYPE = PneumaticsModuleType.CTREPCM;
         public static final int INTAKE_DEPLOY_PCM_CHANNEL = 0;
+        public static final int CLAW_PCM_CHANNEL = 0;
     }
 
 }
