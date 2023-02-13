245cec2fd4de4163678a21f0a9c9b7d240b175e9
Sidd Rao <siddrao.926@gmail.com>
2023-02-13 02:28:51 -0800

    Added Field2d Odometry Output To Shuffleboard

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -19,6 +19,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -28,6 +29,8 @@ public class Swerve extends SubsystemBase {
     public SwerveDrivePoseEstimator swerveOdometry;
     public SwerveModule[] mSwerveMods;
     public Pigeon2 gyro;
+
+    private Field2d field2d;
 
     public Swerve() {
         pcw = new PhotonVisionCameraWrapper();
@@ -44,6 +47,9 @@ public class Swerve extends SubsystemBase {
         };
         //TODO: Set the actual pose
         swerveOdometry = new SwerveDrivePoseEstimator(Constants.Swerve.SWERVE_KINEMATICS, getYaw(), getModulePositions(), new Pose2d());
+
+        field2d = new Field2d();
+        SmartDashboard.putData(field2d);
     }
 
     public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
@@ -136,7 +142,7 @@ public class Swerve extends SubsystemBase {
             );
         }
 
-        System.out.println(swerveOdometry.getEstimatedPosition());
+        field2d.setRobotPose(getPose());
         
         for(SwerveModule mod : mSwerveMods){
             SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Cancoder", mod.getCanCoder().getDegrees());
