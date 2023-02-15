06d1fa1e30a8a8652d1745b0e47276d854e1b810
oliverstivers <oliverstivers@gmail.com>
2023-02-15 14:05:13 -0800

    COMMENTS

--- src/main/java/frc/robot/commands/ArmAndIntakeDefaultCommand.java
+++ src/main/java/frc/robot/commands/ArmAndIntakeDefaultCommand.java
@@ -41,17 +41,29 @@ public class ArmAndIntakeDefaultCommand extends CommandBase {
     public void execute() {
         long temp = endLoop;
         endLoop = System.nanoTime() / (long) Math.pow(10, 9);
+        //gets loop time for consistent arm movement
         long deltaTime = endLoop - temp;
-        intake.setIntakePower(intakeDoubleSupplier.getAsDouble());
+        //sets intake depending on speed and extends and retracts
+        if(intakeDoubleSupplier.getAsDouble() > 0.05){
+            intake.deployIntake();
+            intake.setIntakePower(intakeDoubleSupplier.getAsDouble());
+        }
+        else{
+            intake.retractIntake();
+        }
+        //sets setpoint depending on arm joystick, deltatime, and ticks per second constant, clamped between lower and upper bound
         double setpoint = MathUtil.clamp(pid.getSetpoint() + (armDoubleSupplier.getAsDouble() * deltaTime * Constants.ArmConstants.TICKS_PER_SECOND), ArmConstants.LOWER_BOUND_INTAKE_OUT_TICKS, ArmConstants.UPPER_BOUND_TICKS);
+        //checks if setpoint will kill the robot, if so, schedules command that will deploy intake and update setpoint after half a second to make sure we don't kill the robot
         if ((setpoint < ArmConstants.LOWER_BOUND_INTAKE_IN_TICKS) && !intake.isIntakeOut()) {
             CommandScheduler.getInstance().schedule(new IntakeOutUpdateSetpoint(intake, pid, deltaTime));
         }
+        //if setpoint won't kill the robot, we just set the setpoint
         else{
             pid.setSetpoint(setpoint);
         }
+        //sets the arm power based on pid calculations
         arm.setMotorPower(pid.calculate(arm.getEncoderValue()));
-        
+        //if arm is out of the way, retract intake so we don't bang it into things
         if(arm.getEncoderValue() > Constants.ArmConstants.LOWER_BOUND_INTAKE_IN_TICKS){
             intake.retractIntake();
         }
--- src/main/java/frc/robot/commands/IntakeOutUpdateSetpoint.java
+++ src/main/java/frc/robot/commands/IntakeOutUpdateSetpoint.java
@@ -9,7 +9,9 @@ import frc.robot.subsystems.Intake;
 public class IntakeOutUpdateSetpoint extends SequentialCommandGroup {
     public IntakeOutUpdateSetpoint(Intake intake, PIDController pid, double setpoint) {
         addCommands(
+            //command is scheduled when setpoint will kill intake, so we first extend intake so it doesn't die
                 new InstantCommand(() -> intake.deployIntake()),
+                //after we extend the intake, wait half a secondd so that we make sure we don't kill it
                 new WaitCommand(0.5),
                 new InstantCommand(() -> pid.setSetpoint(setpoint))
                 );
