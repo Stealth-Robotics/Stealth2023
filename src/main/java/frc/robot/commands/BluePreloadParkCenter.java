8d368d226dd20fd38df3857707e6fdbb900493c6
Clementine Allen <tomtinderbundle@gmail.com>
2023-02-20 17:33:03 -0800

    Put master in this brnach, and updated it all

--- src/main/java/frc/robot/commands/BluePreloadParkCenter.java
+++ src/main/java/frc/robot/commands/BluePreloadParkCenter.java
@@ -7,20 +7,20 @@ package frc.robot.commands;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
-import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.Swerve.DrivebaseSubsystem;
 
 public class BluePreloadParkCenter extends SequentialCommandGroup {
   //creates variables for the drivebase and defaultconfig.
-  private final Swerve driveBase;
+  private final DrivebaseSubsystem driveBase;
   private final TrajectoryConfig defaultConfig;    
-  public BluePreloadParkCenter(Swerve driveBase) {
+  public BluePreloadParkCenter(DrivebaseSubsystem driveBase) {
     //assign the drivebase and config file
     this.driveBase = driveBase;
     //sets the config variables to the speed and accel constants.
-    this.defaultConfig = new TrajectoryConfig(Constants.AutoConstants.MAX_SPEED_METERS_PER_SEC, Constants.AutoConstants.MAX_ACCEL_METERS_PER_SEC_SQUARED);
+    this.defaultConfig = new TrajectoryConfig(Constants.AutoConstants.k_MAX_SPEED_MPS, Constants.AutoConstants.k_MAX_ACCEL_MPS_SQUARED);
     addCommands(
       //references the path file and sets the starting color and if the command is running first.
-      new SwerveControllerFollower(driveBase,  "preloadParkCenter", defaultConfig, false, true)
+      new SwerveTrajectoryFollowCommand(driveBase,  "preloadParkCenter", defaultConfig, false, true)
     );
     //grabs any requirements needed for the drivebase from other running commands.
     addRequirements(driveBase);
--- src/main/java/frc/robot/commands/BluePreloadPlusOneRight.java
+++ src/main/java/frc/robot/commands/BluePreloadPlusOneRight.java
@@ -7,21 +7,21 @@ package frc.robot.commands;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
-import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.Swerve.DrivebaseSubsystem;
 
 public class BluePreloadPlusOneRight extends SequentialCommandGroup {
   //creates variables for the drivebase and defaultconfig.
-  private final Swerve driveBase;
+  private final DrivebaseSubsystem driveBase;
   private final TrajectoryConfig defaultConfig;    
-  public BluePreloadPlusOneRight(Swerve driveBase) {
+  public BluePreloadPlusOneRight(DrivebaseSubsystem driveBase) {
     //assign the drivebase and config file
     this.driveBase = driveBase;
     //sets the config variables to the speed and accel constants.
-    this.defaultConfig = new TrajectoryConfig(Constants.AutoConstants.MAX_SPEED_METERS_PER_SEC, Constants.AutoConstants.MAX_ACCEL_METERS_PER_SEC_SQUARED);
+    this.defaultConfig = new TrajectoryConfig(Constants.AutoConstants.k_MAX_SPEED_MPS, Constants.AutoConstants.k_MAX_ACCEL_MPS_SQUARED);
     addCommands(
       //references the path file and sets the starting color and if the command is running first.
-      new SwerveControllerFollower(driveBase,  "preloadPlusOneRight1", defaultConfig, false, true),
-      new SwerveControllerFollower(driveBase,  "preloadPlusOneRight2", defaultConfig, false, false)
+      new SwerveTrajectoryFollowCommand(driveBase,  "preloadPlusOneRight1", defaultConfig, false, true),
+      new SwerveTrajectoryFollowCommand(driveBase,  "preloadPlusOneRight2", defaultConfig, false, false)
     );
     //grabs any requirements needed for the drivebase from other running commands.
     addRequirements(driveBase);
