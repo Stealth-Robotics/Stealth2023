71f7a70991b5741d3cd872d2e8767153adcb7aa0
GreenMachineStudios <116412610+GreenMachineStudios@users.noreply.github.com>
2023-02-27 18:15:52 -0800

    Update BluePreloadParkCenter.java

--- src/main/java/frc/robot/commands/BluePreloadParkCenter.java
+++ src/main/java/frc/robot/commands/BluePreloadParkCenter.java
@@ -20,11 +20,12 @@ public class BluePreloadParkCenter extends SequentialCommandGroup {
     this.defaultConfig = new TrajectoryConfig(Constants.AutoConstants.k_MAX_SPEED_MPS, Constants.AutoConstants.k_MAX_ACCEL_MPS_SQUARED);
     addCommands(
       //references the path file and sets the starting color and if the command is running first.
-      //Start Condition:
-      // - Wrist Straight
-      // - Cone In Gripper
-      // - Rotator At 180
-      // - Telescope Fully Retracted
+      /* Start Condition:
+       - Wrist Straight
+       - Cone In Gripper
+       - Rotator At 180
+       - Telescope Fully Retracted
+      */
 
       //Command Group
 
@@ -33,7 +34,7 @@ public class BluePreloadParkCenter extends SequentialCommandGroup {
       new TelescopeToPosition(null, 100), //TODO: set to actual telescope position.
       new SwerveTrajectoryFollowCommand(driveBase,  "preloadParkCenter", defaultConfig, false, true),
       //Flex Wrist
-      //Gripper Open
+      //Gripper Open 
       //Wrist Straight
       new TelescopeToPosition(null, 0) //TODO: set to actual telescope position.
       //Arm Rotate 90
