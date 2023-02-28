dbca7b38d4d3fd1496dc990c2a1dd9459c28a50e
Clementine Allen <tomtinderbundle@gmail.com>
2023-02-27 16:56:51 -0800

    this should stop the neo from dying
    I am concerened this will prevent it from starting, testing required.

--- src/main/java/frc/robot/subsystems/CrocodileSubsystem.java
+++ src/main/java/frc/robot/subsystems/CrocodileSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
@@ -11,7 +12,7 @@ public class CrocodileSubsystem extends SubsystemBase{
     private final Solenoid wristSolenoid;
     private final Solenoid chomperSolenoid;
     private final CANSparkMax motorA;
-
+    private Debouncer stallDebouncer = new Debouncer(0.050, Debouncer.DebounceType.kRising);
     private double currentMotorPower;
     public CrocodileSubsystem() {
         motorA = new CANSparkMax(RobotMap.EndEffector.END_EFFECTOR_MOTOR_A, MotorType.kBrushless);
@@ -27,9 +28,12 @@ public class CrocodileSubsystem extends SubsystemBase{
 
     public void setMotorSpeed(double speed){
         motorA.set(speed);
+        currentMotorPower = speed;
     }
 
-
+    public double getMotorVelocity(){
+        return motorA.getEncoder().getVelocity();
+    }
     private void setWrist(boolean newValue){
         wristSolenoid.set(newValue);
     }
@@ -75,6 +79,8 @@ public class CrocodileSubsystem extends SubsystemBase{
 
     @Override
     public void periodic() {
-        
+        if (currentMotorPower > 0  && stallDebouncer.calculate((getMotorVelocity() < 50))) {
+            setMotorSpeed(0);
+        }
     }
 }
