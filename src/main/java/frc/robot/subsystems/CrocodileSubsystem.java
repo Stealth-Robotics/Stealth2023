c36b964f343b5cbf89cc9372a9d073185b870528
Clementine Allen <tomtinderbundle@gmail.com>
2023-02-27 16:37:09 -0800

    Update CrocodileSubsystem.java

--- src/main/java/frc/robot/subsystems/CrocodileSubsystem.java
+++ src/main/java/frc/robot/subsystems/CrocodileSubsystem.java
@@ -11,6 +11,8 @@ public class CrocodileSubsystem extends SubsystemBase{
     private final Solenoid wristSolenoid;
     private final Solenoid chomperSolenoid;
     private final CANSparkMax motorA;
+
+    private double currentMotorPower;
     public CrocodileSubsystem() {
         motorA = new CANSparkMax(RobotMap.EndEffector.END_EFFECTOR_MOTOR_A, MotorType.kBrushless);
         wristSolenoid = new Solenoid(
@@ -27,33 +29,52 @@ public class CrocodileSubsystem extends SubsystemBase{
         motorA.set(speed);
     }
 
+
+    private void setWrist(boolean newValue){
+        wristSolenoid.set(newValue);
+    }
+
     public void toggleWrist(){
         wristSolenoid.toggle();
     }
+    
+    public boolean getWristPositionBool(){
+        return wristSolenoid.get();
+    }
 
-    public void setWrist(boolean newValue){
-        wristSolenoid.set(newValue);
+    public void wristUp(){
+        setWrist(true);
+    }
+
+    public void wristDown(){
+        setWrist(false);
+    }
+        
+
+    private void setChomper(boolean newValue){
+        chomperSolenoid.set(newValue);
     }
 
     public void toggleChomper(){
         chomperSolenoid.toggle();
     }
 
-    public void setChomper(boolean newValue){
-        chomperSolenoid.set(newValue);
-    }
-
-    public void close()
+    public void closeChomper()
     {
-        setWrist(false);
+        setChomper(true);
     }
 
-    public void open()
+    public void openChomper()
     {
-        setWrist(true);
+        setChomper(false);
     }
 
-    public boolean getIsOpen(){
-        return wristSolenoid.get();
+    public boolean getChomperPositionBool(){
+        return chomperSolenoid.get();
+    }
+
+    @Override
+    public void periodic() {
+        
     }
 }
