34d7f1a703b22a606e12b28b86ddedeeee1fc53d
Clementine Allen <tomtinderbundle@gmail.com>
2023-02-27 17:49:02 -0800

    Added arm death protection to the command

--- src/main/java/frc/robot/commands/RotatorToPosition.java
+++ src/main/java/frc/robot/commands/RotatorToPosition.java
@@ -1,24 +1,38 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.RotatorSubsystem;
+import frc.robot.subsystems.TelescopeSubsystem;
 
 public class RotatorToPosition extends CommandBase {
     private final RotatorSubsystem rotatorSubsystem;
+    private final TelescopeSubsystem telescope;
     private final double setpoint;
-    public RotatorToPosition(RotatorSubsystem rotatorSubsystem, double setpoint)
+    private final ResetTelescope resetTelescope;
+    private boolean resettingElevator;
+    public RotatorToPosition(RotatorSubsystem rotatorSubsystem, TelescopeSubsystem telescope, double setpoint)
     {
         this.rotatorSubsystem = rotatorSubsystem;
         this.setpoint = setpoint;
+        this.telescope = telescope;
         addRequirements(rotatorSubsystem);
+
+        resetTelescope = new ResetTelescope(telescope);
     }
     @Override
     public void initialize() {
-        rotatorSubsystem.setSetpoint(setpoint);
+        if (telescope.getCurrentPosition() > 2048 && Math.abs(telescope.getCurrentPosition() - setpoint) > 4096){
+            CommandScheduler.getInstance().schedule(resetTelescope.andThen(new RotatorToPosition(rotatorSubsystem, telescope, setpoint)));
+            resettingElevator = true;
+        }
+        else {
+            rotatorSubsystem.setSetpoint(setpoint);
+        }
     }
     @Override
     public boolean isFinished() {
-        return rotatorSubsystem.getMeasurement() == setpoint;
+        return rotatorSubsystem.getMeasurement() == setpoint || resettingElevator;
     }
     @Override
     public void end(boolean interrupted) {
