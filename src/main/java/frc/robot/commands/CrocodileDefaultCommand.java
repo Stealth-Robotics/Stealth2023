2b21f7ee358b7aa2e383169557cb00e1db85110a
Clementine Allen <tomtinderbundle@gmail.com>
2023-03-01 15:52:13 -0800

    added slow mode and other PR fixes

--- src/main/java/frc/robot/commands/CrocodileDefaultCommand.java
+++ src/main/java/frc/robot/commands/CrocodileDefaultCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -8,16 +9,26 @@ import frc.robot.subsystems.CrocodileSubsystem;
 public class CrocodileDefaultCommand extends CommandBase {
     private final CrocodileSubsystem subsystem;
     private final DoubleSupplier motorSpeed;
+    private final DoubleSupplier reverseSpeed;
+    private final BooleanSupplier slowMovement;
 
-    public CrocodileDefaultCommand(CrocodileSubsystem subsystem,DoubleSupplier motorSpeed) {
+    public CrocodileDefaultCommand(CrocodileSubsystem subsystem, DoubleSupplier motorSpeed, DoubleSupplier reverseSpeed, BooleanSupplier slowMovement) {
         this.subsystem = subsystem;
         this.motorSpeed = motorSpeed;
+        this.reverseSpeed = reverseSpeed;
+        this.slowMovement = slowMovement;
         addRequirements(subsystem);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        subsystem.setMotorSpeed(motorSpeed.getAsDouble());
+        if (slowMovement.getAsBoolean()) {
+            subsystem.setMotorSpeed(motorSpeed.getAsDouble() * 0.2);
+            return;
+        }
+        double forward = motorSpeed.getAsDouble();
+        double reverse = -1 * reverseSpeed.getAsDouble();
+        subsystem.setMotorSpeed(forward + reverse);
     }
 }
