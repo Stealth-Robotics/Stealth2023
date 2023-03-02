af1d87c51acd45c585dd51374435f67db63b2a12
Clementine Allen <tomtinderbundle@gmail.com>
2023-03-02 09:10:05 -0800

    added a pulsing leveler

--- src/main/java/frc/robot/commands/LevelRobotPulses.java
+++ src/main/java/frc/robot/commands/LevelRobotPulses.java
@@ -1,31 +1,42 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RepeatCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Constants;
 import frc.robot.subsystems.Swerve.DrivebaseSubsystem;
 
-public class LevelRobotPulses extends SequentialCommandGroup{
+public class LevelRobotPulses extends CommandBase{
     private final DrivebaseSubsystem drive;
-    
+    private double timer = 0;
+    private double totalError;
     public LevelRobotPulses(DrivebaseSubsystem drivetrain){
+
         drive = drivetrain;
         addRequirements(drive);
-        addCommands(
-            new RepeatCommand(
-                new SequentialCommandGroup(
-                    new InstantCommand(() -> drivetrain.drive(
-                        new Translation2d(1, 0), 0, true, true)),
-                    new WaitCommand(0.1))
-            )   
-        );
+        
+        timer = System.currentTimeMillis();
+        totalError = drive.getPitchAsDouble() + drive.getRollAsDouble();
     }
-
-   
+    @Override
+    public void initialize() {
+        
+    }
+    @Override
+    public void execute() {
+        double temp = timer;
+        timer = System.currentTimeMillis();
+        double delta = timer - temp; 
+        totalError = drive.getPitchAsDouble() + drive.getRollAsDouble();
+        double direction = -Integer.signum((int)totalError);
+        if (delta > 500){
+            drive.drive(new Translation2d(1 * direction, 0), 0, true, true);
+        }
+    }
+    @Override
+    public boolean isFinished() { 
+        return Math.abs(totalError) < 5;
+    }
+    @Override
+    public void end(boolean interrupted) {
+        drive.drive(new Translation2d(0, 0), 0, true, true);
+    }   
 }
