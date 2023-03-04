5cc32b38dc11f9b3fbb27d9675c52ddaad938b4c
Clementine Allen <tomtinderbundle@gmail.com>
2023-03-04 09:37:14 -0800

    Update LowPresetSequence.java

--- src/main/java/frc/robot/commands/LowPresetSequence.java
+++ src/main/java/frc/robot/commands/LowPresetSequence.java
@@ -9,7 +9,7 @@ import frc.robot.subsystems.TelescopeSubsystem;
 public class LowPresetSequence extends SequentialCommandGroup {
     public LowPresetSequence(TelescopeSubsystem telescope, RotatorSubsystem rotator, CrocodileSubsystem crocodile) {
         addCommands(
-            new RotatorToPosition(rotator, telescope, 80),
+            new RotatorToPosition(rotator, telescope, 70),
             new TelescopeToPosition(telescope, 10000),
             new RunCrocodileMotors(crocodile, -0.2),
             new InstantCommand(()->crocodile.openChomper())
