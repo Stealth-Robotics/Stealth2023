ee4dfe1bd576cb1c7945083f245a861124f2b7b0
GreenMachineStudios <116412610+GreenMachineStudios@users.noreply.github.com>
2023-03-08 16:14:03 -0800

    Work on Intake

--- src/main/java/frc/robot/subsystems/CrocodileSubsystem.java
+++ src/main/java/frc/robot/subsystems/CrocodileSubsystem.java
@@ -1,92 +1,65 @@
 package frc.robot.subsystems;
 
+import javax.swing.text.Position;
+
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import edu.wpi.first.math.filter.Debouncer;
-import edu.wpi.first.wpilibj.Solenoid;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.RobotMap;
 
 public class CrocodileSubsystem extends SubsystemBase {
-    private final Solenoid wristSolenoid;
-    private final Solenoid chomperSolenoid;
     private final WPI_TalonFX intake;
-    private Debouncer stallDebouncer = new Debouncer(0.050, Debouncer.DebounceType.kRising);
-    private double currentMotorPower;
+    private final WPI_TalonFX wrist;
+    private final PIDController pid;
+    private final DutyCycleEncoder encoder;
+    private final DigitalInput distanceSensor;
 
     public CrocodileSubsystem() {
         intake = new WPI_TalonFX(RobotMap.Crocodile.INTAKE);
+        wrist = new WPI_TalonFX(RobotMap.Crocodile.WRIST);
+        pid = new PIDController(0, 0, 0);
+        encoder = new DutyCycleEncoder(1);
+        distanceSensor = new DigitalInput(2);
         intake.setNeutralMode(NeutralMode.Brake);
+        wrist.setNeutralMode(NeutralMode.Brake);
         /* enabled | Limit(amp) | Trigger Threshold(amp) | Trigger Threshold Time(s) */
         intake.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 35, 60, 0.1));
+        setSetpoint(encoder.getAbsolutePosition());
 
-        wristSolenoid = new Solenoid(
-                RobotMap.Pneumatics.PCM,
-                RobotMap.Pneumatics.PCM_TYPE,
-                RobotMap.Pneumatics.CLAW_PCM_CHANNEL);
-        chomperSolenoid = new Solenoid(
-                RobotMap.Pneumatics.PCM,
-                RobotMap.Pneumatics.PCM_TYPE,
-                RobotMap.Pneumatics.CHOMPER_PCM_CHANNEL);
     }
 
-    public void setMotorSpeed(double speed) {
+    public void setIntakeSpeed(double speed) {
         intake.set(speed);
-        currentMotorPower = speed;
     }
 
-    public double getMotorVelocity() {
-        return intake.getSelectedSensorVelocity();
+    private void setWristSpeed(double speed) {
+        wrist.set(speed);
     }
 
-    private void setWrist(boolean newValue) {
-        wristSolenoid.set(newValue);
+    public void setSetpoint(double position) {
+        pid.setSetpoint(position);
     }
 
-    public void toggleWrist() {
-        wristSolenoid.toggle();
+    public double getSetpoint() {
+        return pid.getSetpoint();
     }
 
-    public boolean getWristPositionBool() {
-        return wristSolenoid.get();
+    public double getAbsolutePosition() {
+        return encoder.getAbsolutePosition();
     }
 
-    public void wristUp() {
-        setWrist(true);
-    }
-
-    public void wristDown() {
-        setWrist(false);
-    }
-
-    private void setChomper(boolean newValue) {
-        chomperSolenoid.set(newValue);
-    }
-
-    public void toggleChomper() {
-        chomperSolenoid.toggle();
-    }
-
-    public void closeChomper() {
-        setChomper(true);
-    }
-
-    public void openChomper() {
-        setChomper(false);
-    }
-
-    public boolean getChomperPositionBool() {
-        return chomperSolenoid.get();
+    public boolean getDistanceSensor() {
+        return distanceSensor.get();
     }
 
     @Override
     public void periodic() {
-        // if (currentMotorPower > 0 && stallDebouncer.calculate((getMotorVelocity() <
-        // 50))) {
-        // setMotorSpeed(0);
-        // }
+        setWristSpeed(pid.calculate(encoder.getAbsolutePosition()));
     }
 }
