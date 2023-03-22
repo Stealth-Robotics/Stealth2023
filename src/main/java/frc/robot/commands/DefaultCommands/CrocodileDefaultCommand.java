f3879b0e52cb92d56e032bd0a77e6acb2275b98a
Sidd Rao <siddrao.926@gmail.com>
2023-03-22 12:19:08 -0700

    Added Candle To Intake Default

--- src/main/java/frc/robot/commands/DefaultCommands/CrocodileDefaultCommand.java
+++ src/main/java/frc/robot/commands/DefaultCommands/CrocodileDefaultCommand.java
@@ -8,7 +8,9 @@ import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.CandleSubsystem;
 import frc.robot.subsystems.CrocodileSubsystem;
+import frc.robot.subsystems.CrocodileSubsystem.GamePiece;
 
 public class CrocodileDefaultCommand extends CommandBase {
     private final CrocodileSubsystem subsystem;
@@ -17,19 +19,29 @@ public class CrocodileDefaultCommand extends CommandBase {
     private boolean beamBreakTracker = false;
     private final Debouncer debouncer;
     private final Timer timer;
+    private final CandleSubsystem candleSubsystem;
     DoubleConsumer giveHapticFeedback;
 
-    public CrocodileDefaultCommand(CrocodileSubsystem subsystem, DoubleSupplier trigger, DoubleSupplier manualWrist,
+    public CrocodileDefaultCommand(CrocodileSubsystem subsystem, CandleSubsystem candle, DoubleSupplier trigger, DoubleSupplier manualWrist,
             DoubleConsumer giveHapticFeedback) {
         this.subsystem = subsystem;
         this.trigger = trigger;
         this.wristTrigger = manualWrist;
+        this.candleSubsystem = candle;
         timer = new Timer();
         
         debouncer = new Debouncer(0.5, DebounceType.kFalling);
         this.giveHapticFeedback = giveHapticFeedback;
-        addRequirements(subsystem);
+        
+        addRequirements(subsystem, candle);
     }
+
+    public CrocodileDefaultCommand(CrocodileSubsystem subsystem, DoubleSupplier trigger, DoubleSupplier manualWrist,
+            DoubleConsumer giveHapticFeedback) {
+        this(subsystem, null, trigger, manualWrist, giveHapticFeedback);
+    }
+
+    
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
@@ -59,6 +71,15 @@ public class CrocodileDefaultCommand extends CommandBase {
             // get current time and do something to rumble
             timer.start();
             beamBreakTracker = true;
+
+            if(subsystem.getGamePiece() == GamePiece.CONE)
+            {
+                candleSubsystem.coneSolid();
+            }
+
+            else {
+                candleSubsystem.cubeSolid();
+            }
 
             giveHapticFeedback.accept(1.0);
         }
